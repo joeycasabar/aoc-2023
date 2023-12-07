4d720373e9e9ce1aad23618ef43d266861645156
Joey Casabar <joeycasabar@gmail.com>
2023-12-07 15:51:51 -0800

    added location extraction code

--- code/d03.py
+++ code/d03.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 import argparse
+symbols = "#$%&*+-/=@"
+numbers = "0123456789"
+
+max_height = 140
+max_width = 140
 
 parser = argparse.ArgumentParser(
     prog='Advent Of Code - Day 3',
@@ -11,6 +16,30 @@ parser.add_argument('input')
 
 args = parser.parse_args()
 
+
+def get_locations(map, charset):
+    locations = []
+    for c in charset:
+        for y in range(0, 140, 1):
+            row = map[y]
+            if c in row:
+                x = row.index(c)
+                locations.append(
+                    {
+                        'symbol': c,
+                        'x': x + 1,
+                        'y': y + 1
+                    }
+                )
+    return locations
+
+
 with open(args.input) as input:
+    map = []
     for line in input:
-        print(line.rstrip())
+        map.append(line.rstrip())
+    # print(map)
+    symbols_xy = get_locations(map, symbols)
+    print(symbols_xy)
+    numbers_xy = get_locations(map, numbers)
+    print(numbers_xy)
